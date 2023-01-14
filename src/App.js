dd39c329e9b8f7a6435d6b28d0e787f84511600b
Ugurcan Kaya <118749864+ugrcnkaya@users.noreply.github.com>
2023-01-14 14:27:37 +0100

    React Component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 const Person = () => {
   return (
       <>
-      <h1>Name : John</h1>
-        <h2>Last Name: Doe</h2>
+      <h1>Name : Ugur</h1>
+        <h2>Last Name: Kaya</h2>
           <h2>Age : 30</h2>
+          <hr />
       </>
   )
 }
@@ -18,7 +19,12 @@ const isUserLoggedIn = true;
     <div className="App">
     <h1>{isUserLoggedIn ? (<>
       <h1>Logged in</h1>
-            <Person></Person>
+            <Person />
+            <Person />
+            <Person />
+            <Person />
+
+
     </>) :
         (<>
         <h1>Not Logged In</h1>
