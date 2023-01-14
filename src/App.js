e5f380bfd1c31d579059962d96a5f3d2c54ce407
Ugurcan Kaya <118749864+ugrcnkaya@users.noreply.github.com>
2023-01-14 14:34:43 +0100

    React Probs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import './App.css';
-
-
-const Person = (props) => {
-  return (
-      <>
-      <h1>Name : {props.name}</h1>
-        <h2>Last Name: {props.lastname}</h2>
-          <h2>Age : {props.age}</h2>
-          <hr />
-      </>
-  )
-}
+//React State
+import {useState} from 'react'
 
 
 function App() {
-const isUserLoggedIn = true;
+const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-    <h1>{isUserLoggedIn ? (<>
-      <h1>Logged in</h1>
-            <Person  name={'John'} lastname={'Doe'} age={ 35}/>
-            <Person />
-            <Person />
-            <Person />
-
-
-    </>) :
-        (<>
-        <h1>Not Logged In</h1>
-          <h2>Login Form</h2>
-        </>)}</h1>
-
+    <button>-</button>
+      <h1>{counter}</h1>
+      <button>+</button>
     </div>
   );
 }
