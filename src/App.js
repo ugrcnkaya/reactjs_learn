b055c43055a91f018fbd7b2ce5ab1e4932244c20
Ugurcan Kaya <118749864+ugrcnkaya@users.noreply.github.com>
2023-01-14 14:26:46 +0100

    React Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 
+
+const Person = () => {
+  return (
+      <>
+      <h1>Name : John</h1>
+        <h2>Last Name: Doe</h2>
+          <h2>Age : 30</h2>
+      </>
+  )
+}
+
+
 function App() {
-const isUserLoggedIn = false;
+const isUserLoggedIn = true;
   return (
     <div className="App">
     <h1>{isUserLoggedIn ? (<>
       <h1>Logged in</h1>
+            <Person></Person>
     </>) :
         (<>
         <h1>Not Logged In</h1>
